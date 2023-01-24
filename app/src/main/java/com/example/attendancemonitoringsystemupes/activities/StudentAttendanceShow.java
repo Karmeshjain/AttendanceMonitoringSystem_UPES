ed84ef17466d6bff14e5c77fc4054defcb716553
Karmeshjain <jainkarmesh1@gmail.com>
2023-01-24 14:56:27 +0530

    "Layout changes and Attendance Show class changes"

--- app/src/main/java/com/example/attendancemonitoringsystemupes/activities/StudentAttendanceShow.java
+++ app/src/main/java/com/example/attendancemonitoringsystemupes/activities/StudentAttendanceShow.java
@@ -2,7 +2,10 @@ package com.example.attendancemonitoringsystemupes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.attendancemonitoringsystemupes.R;
 
@@ -12,5 +15,17 @@ public class StudentAttendanceShow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_attendance_show);
+        View attendanceColourView = findViewById(R.id.attendanceColour);
+        Intent intent = getIntent();
+        String attendanceJsonStudent= intent.getStringExtra("result_string");
+        int present=0;
+        if(present==1)
+        {
+            attendanceColourView.setBackgroundColor(Color.GREEN);
+        }
+        else
+        {
+            attendanceColourView.setBackgroundColor(Color.RED);
+        }
     }
 }
