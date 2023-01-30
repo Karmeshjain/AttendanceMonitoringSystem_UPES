893bad6c8abdad2df40524d81b2ff5c02adb39c5
Karmeshjain <jainkarmesh1@gmail.com>
2023-01-31 02:21:35 +0530

    "Image Sender Api calling"

--- app/src/main/java/com/example/attendancemonitoringsystemupes/activities/AttendanceFaculty.java
+++ app/src/main/java/com/example/attendancemonitoringsystemupes/activities/AttendanceFaculty.java
@@ -18,15 +18,28 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.attendancemonitoringsystemupes.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,10 +47,7 @@ import java.util.List;
 
 public class AttendanceFaculty extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST_CODE = 1;
     private ArrayList<Uri> imageUris = new ArrayList<>();
-    Uri photoURI;
-    String jsonString;
     private String courseName;
     private String facultyName;
     private String roomNo;
@@ -48,7 +58,7 @@ public class AttendanceFaculty extends AppCompatActivity {
     private List<Bitmap> imageList = new ArrayList<>();
     private ImageAdapter imageAdapter;
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
-
+    private JSONObject attendanceListJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,16 +89,53 @@ public class AttendanceFaculty extends AppCompatActivity {
 //        jsonString = intent.getStringExtra("result_string");
 //        parseJsonString(jsonString);
       //  takeMultiplePhotos();
-        String result=sendPhotosToApi();
         submitPhotosButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code to execute when the button is clicked
+                ImageSenderApiCall();
                 Intent showList = new Intent(AttendanceFaculty.this, AttendanceList.class);
-                showList.putExtra("result_string", result);
+                showList.putExtra("result_string", String.valueOf(attendanceListJson));
                 startActivity(showList);
             }
         });
+    }
+    private void ImageSenderApiCall()
+    {
+        List<String> base64List = new ArrayList<>();
+        for (Bitmap bitmap : imageList) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            base64List.add(encoded);
+        }
+        JSONObject json = new JSONObject();
+        try {
+            json.put("images", new JSONArray(base64List));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String url = "API_ENDPOINT";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // handle the response here
+                        attendanceListJson=response;
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // handle the error here
+                        Toast.makeText(AttendanceFaculty.this, "API Fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        queue.add(jsonObjectRequest);
+
     }
     private void openCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -108,14 +155,6 @@ public class AttendanceFaculty extends AppCompatActivity {
         }
     }
 
-
-
-    private String sendPhotosToApi()
-    {
-        String result="";
-
-       return result;
-    }
 
     private void parseJsonString(String jsonString)
     {
