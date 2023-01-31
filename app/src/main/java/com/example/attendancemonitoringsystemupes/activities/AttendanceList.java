e7c7381e5e59b7d949a5ebabfd92357963dfa0e0
Karmeshjain <jainkarmesh1@gmail.com>
2023-01-31 18:44:24 +0530

    "Absent Present List Class Addition"

--- app/src/main/java/com/example/attendancemonitoringsystemupes/activities/AttendanceList.java
+++ app/src/main/java/com/example/attendancemonitoringsystemupes/activities/AttendanceList.java
@@ -4,21 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.attendancemonitoringsystemupes.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AttendanceList extends AppCompatActivity {
 
     private TextView studentList;
-
+    private String jsonString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_list);
-        Intent intent = getIntent();
-        String result = intent.getStringExtra("result_string");
-        studentList=findViewById(R.id.showAttendance);
-        studentList.setText(result);
+//        Intent intent = getIntent();
+//        String result = intent.getStringExtra("result_string");
+       studentList=findViewById(R.id.showAttendance);
+//        studentList.setText(result);
+
+        try {
+            InputStream is = getResources().openRawResource(R.raw.attendancelist);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonString = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+       Log.d("json",jsonString);
+
     }
 }
