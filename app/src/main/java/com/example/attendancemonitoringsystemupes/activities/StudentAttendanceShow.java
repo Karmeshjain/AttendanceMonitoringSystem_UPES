ed84106fc12586136f17e2f35e9f295cd2cca2cd
Karmeshjain <jainkarmesh1@gmail.com>
2023-02-01 12:58:12 +0530

    "Student Attendance Status Api Class Attendance From Server"

--- app/src/main/java/com/example/attendancemonitoringsystemupes/activities/StudentAttendanceShow.java
+++ app/src/main/java/com/example/attendancemonitoringsystemupes/activities/StudentAttendanceShow.java
@@ -6,21 +6,34 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.attendancemonitoringsystemupes.R;
+import com.example.attendancemonitoringsystemupes.apiCalls.CoursesTaskApi;
+import com.example.attendancemonitoringsystemupes.apiCalls.StudentAttendanceStatusApi;
+
+import java.util.concurrent.ExecutionException;
 
 public class StudentAttendanceShow extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_attendance_show);
         View attendanceColourView = findViewById(R.id.attendanceColour);
         Intent intent = getIntent();
         String courseName= intent.getStringExtra("result_string");
-
-        int present=0;
-        if(present==1)
+        StudentAttendanceStatusApi studentAttendanceStatusApi = new StudentAttendanceStatusApi(this);
+        String result="";
+        try {
+            result = studentAttendanceStatusApi.execute(MainActivity.token,courseName).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(result=="true")
         {
             attendanceColourView.setBackgroundColor(Color.GREEN);
         }
